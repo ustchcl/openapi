4a54fc9a192de543f55feac322085174e712e122
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-01-26 21:34:16 +0000

    Deploy softprops/openapi to github.com/softprops/openapi.git:gh-pages

---
+++ implementors/core/fmt/trait.Display.js
@@ -0,0 +1,9 @@
+(function() {var implementors = {};
+implementors["openapi"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"openapi/error/enum.Error.html\" title=\"enum openapi::error::Error\">Error</a>",synthetic:false,types:["openapi::error::Error"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        })()
---
+++ implementors/failure/trait.Fail.js
@@ -0,0 +1,9 @@
+(function() {var implementors = {};
+implementors["openapi"] = [{text:"impl Fail for <a class=\"enum\" href=\"openapi/error/enum.Error.html\" title=\"enum openapi::error::Error\">Error</a>",synthetic:false,types:["openapi::error::Error"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        })()
